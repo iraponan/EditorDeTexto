3720db3fc66305eab76f25f66fed2c4ca357f380
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2021-10-10 17:08:33 -0300

    Iniciando o projeto

--- EditorDeTexto/Program.cs
+++ EditorDeTexto/Program.cs
@@ -3,7 +3,32 @@
 namespace EditorDeTexto {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("3 - Sair");
+
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch(opcao) {
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                case 3: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir() {
+
+        }
+
+        static void Editar() {
+
         }
     }
 }
