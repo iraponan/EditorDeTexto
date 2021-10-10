6628cb74c4e807b9dcfeedd41c9b485b30a83bfd
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2021-10-10 17:39:26 -0300

    Abrindo um arquivo

--- EditorDeTexto/Program.cs
+++ EditorDeTexto/Program.cs
@@ -26,7 +26,19 @@ namespace EditorDeTexto {
         }
 
         static void Abrir() {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
 
+            string caminho = Console.ReadLine();
+
+            using (var arquivo = new StreamReader(caminho)) {
+                string texto = arquivo.ReadToEnd();
+                Console.WriteLine(texto);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+            Menu();
         }
 
         static void Editar() {
